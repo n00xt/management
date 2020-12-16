86830229457535e06fac366cf3162fdccccf97b0
kbal <kbal@ekoltech.sk>
2020-12-16 03:36:03 +0100

    Init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          EKOLTECH - Prehľady
+        </q-toolbar-title>
+
+        <div>v1.0-beta</div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page padding>
+    <div class="row q-ma-lg">
+      <div class="col-12">
+        <q-btn @click="openMetabase('MTZ')" color="primary" size="25px" class="full-width" rounded padding="lg" label="MTZ"></q-btn>
+      </div>
+    </div>
+    <div class="row q-ma-lg">
+      <div class="col-12">
+        <q-btn @click="openMetabase('PREDAJ')" color="primary" size="25px" class="full-width" rounded padding="lg" label="PREDAJ"></q-btn>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+export default {
+  name: 'PageIndex',
+  methods: {
+    openMetabase (dashboard) {
+      if (dashboard === 'MTZ') {
+        openURL('http://10.0.11.5:3001/public/dashboard/531c1cbb-e876-404a-b6fd-56d4e34fd4a6')
+      } else if (dashboard === 'PREDAJ') {
+        openURL('http://10.0.11.5:3001/public/dashboard/d5150a72-7683-4c46-8b30-b0e86334991d')
+      }
+    }
+  }
+}
+</script>
